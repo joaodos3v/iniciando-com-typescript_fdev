15919cab8bd5436287efb196759341d4323ad060
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-01-26 08:24:34 -0300

    feat: type like enum (9)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // import './types/variables';
-//import './types/array';
+// import './types/array';
 
-//import './types/enums';
-import './types/enums_const';
+// import './types/enums';
+// import './types/enums_const';
+import './types/enums_type';
---
+++ src/types/enums_type.ts
@@ -0,0 +1,4 @@
+type StatusVenda = 'iniciada' | 'concluida' | 'cancelada';
+
+let st: StatusVenda = 'concluida';
+// st = 'opa'; -> Type '"opa"' is not assignable to type 'StatusVenda'.
