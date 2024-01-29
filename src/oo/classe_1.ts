e90f7cab4e4cb0f91a326ed8ae6d50f3f0183621
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-01-29 12:05:28 -0300

    feat: access modifiers (25)

--- src/oo/classe_1.ts
+++ src/oo/classe_1.ts
@@ -1,8 +1,22 @@
 class Course {
-  name: string | undefined;
+  private _id: number | undefined;
+  name: string | undefined; // Se quisermos deixar mais claro que é um atributo público, podemos adicionar "public" antes do nome
+  public email: string | undefined;
+
+  get id() {
+    return this._id;
+  }
+
+  set id(id: number | undefined) {
+    if (id && id > 0) {
+      this._id = id;
+    }
+  }
 }
 
 // Só por definir uma classe, já podemos usar a palavra reservada "new".
 const c1 = new Course();
+c1.id = 123;
 c1.name = 'Iniciando com TypeScript';
-console.log(c1);
+console.log(c1.id);
+console.log(c1.name);
