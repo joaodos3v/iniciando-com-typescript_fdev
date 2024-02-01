8d1c8e8ace2bea17464b84808f7cbd3cad96371d
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-01 07:52:49 -0300

    feat: optional properties in interfaces (36)

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -2,6 +2,7 @@ interface User {
   id: number;
   name: string;
   email: string;
+  pass?: string;
   // funcao: (a: number, b: number) => boolean // Existe a possibilidade de "obrigarmos" que, na implementação, sejam criadas funções também
 }
 
@@ -11,8 +12,19 @@ const authenticatedUser: User = {
   email: 'jjvvro@zmail.com',
 };
 
+const userWithPassword: User = {
+  id: 1,
+  name: 'João Vitor',
+  email: 'jjvvro@zmail.com',
+  pass: '123456',
+};
+
 console.log(authenticatedUser.id);
 console.log(authenticatedUser.name);
 console.log(authenticatedUser.email);
+
+console.log(userWithPassword.id);
+console.log(userWithPassword.name);
+console.log(userWithPassword.email);
 
 export {};
